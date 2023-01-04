b5f9e287f2c51fa22e3240559c00c48c9ae3a7db
ymon3103 <ymonbaron@outlook.com>
2023-01-04 15:15:35 +0100

    Add IAmRich app

---
+++ IAmRich/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# The Complete Development Bootcamp
+**By [The App Brewery](https://www.appbrewery.co/).**
+
+Course followed on [Udemy](https://www.udemy.com/course/flutter-bootcamp-with-dart). These are the Android applications that I coded while following the course.
+
+### [I am Rich](./IAmRich/)
+An app to show off how rich I am.<br>
+<img src="Screenshots/IAmRich.jpg" width="100">
